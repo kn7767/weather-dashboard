520458ae124b257ffbc190ae81ecd52a34af9d77
kn7767 <112599359+kn7767@users.noreply.github.com>
2022-11-14 23:11:49 -0800

    5 day added, need icons

--- script.js
+++ script.js
@@ -23,7 +23,14 @@ function getWeatherTest() {
     .then(response => {
         console.log(response)
     })
-}
+//5 day forecast
+    let forecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + "Denver" + "&appid=" + key; 
+    fetch(forecast)
+    .then((response) => response.json())
+    .then(response => {
+        console.log(response)    
+    })
+};
 getWeatherTest()
 
 //getting weather
@@ -53,40 +60,110 @@ function todayWeather (cityName) {
     $('#date').text(today.format('L'));
 
     //five day section 
-    //url error :(
-    let forecastURL =  "api.openweathermap.org/data/2.5/forecast?id=" + cityName + "&appid=" + APIkey; 
+    let forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIkey + "&APPID=" + APIkey + "&units=imperial";
     fetch(forecastURL)
     .then((response) => response.json())
     .then(response => {
         console.log(response)
-        var forecastEl = document.querySelector('.future'); 
-        for (i = 0; i < forecastEl; i++) {
-    //         var forecastIndex = i * 8 + 4;
-    //         var forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
-    //         var forecastDay = forecastDate.getDate();
-    //         var forecastMonth = forecastDate.getMonth() + 1;
-    //         var forecastYear = forecastDate.getFullYear();
-    //         var forecastDateEl = document.createElement("h5");
-    //         forecastDateEl.setAttribute("class", "forecastdate")
-    //         forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
-    //         forecastEl[i].append(forecastDateEl);
+        var forecast1 = document.querySelector('.day1'); 
+        var forecast2 = document.querySelector('.day2'); 
+        var forecast3 = document.querySelector('.day3'); 
+        var forecast4 = document.querySelector('.day4'); 
+        var forecast5 = document.querySelector('.day5');
+        
+        //day 1 forcast
+        let icon1 = response.list[0].weather[0].icon;
+        var iconEl1 = document.createElement("img");
+        iconEl1.setAttribute("src", "http://openweathermap.org/img/wn/" + icon1 + "@2x.png");
+        iconEl1.setAttribute("alt", response.list[0].weather[0].description);
+        forecast1.append(icon1);
 
-    //commenting out until url works 
-            // forecastTempEl = document.createElement("p");
-            // forecastTempEl.innerHTML = "Temp: " + response.data.list[forecastIndex].main.temp;
-            // forecastEl[i].append(forecastTempEl);
-            // forecastHumidEl = document.createElement("p");
-            // forecastHumidEl.innerHTML = "Humidity: " + response.data.list[forecastIndex].main.humidity + "%";
-            // forecastEl[i].append(forecastHumidEl);
-            // forecastWindEl = document.createElement("p");
-            // forecastWindEl.innerHTML = "Wind Speed: " + response.data.list[forecastIndex].wind.speed;
-            // forecastEl[i].append(forecastWindEl);
-        }
-    })
+        var tempEl1 = document.createElement("p");
+        var windEl1 = document.createElement("p");
+        var humidEl1 = document.createElement("p");
+
+        tempEl1.innerHTML = "Temp: " + response.list[0].main.temp + "F";
+        forecast1.append(tempEl1)
+        windEl1.innerHTML = "Wind: " + response.list[0].wind.speed + " MPH";
+        forecast1.append(windEl1)
+        humidEl1.innerHTML = "Humidity: " + response.list[0].main.humidity + "%";
+        forecast1.append(humidEl1)
+        
+        //day 2 forecast
+        var iconEl2 = document.createElement("img");
+        let icon2 = response.list[1].weather[0].icon;
+        iconEl2.setAttribute("src", "https://openweathermap.org/img/wn/" + icon2 + "@2x.png");
+        iconEl2.setAttribute("alt", response.list[0].weather[0].description);
+        forecast2.append(icon2);
+
+        var tempEl2 = document.createElement("p");
+        var windEl2 = document.createElement("p");
+        var humidEl2 = document.createElement("p");
+
+        tempEl2.innerHTML = "Temp: " + response.list[1].main.temp + "F";
+        forecast2.append(tempEl2)
+        windEl2.innerHTML = "Wind: " + response.list[1].wind.speed + " MPH";
+        forecast2.append(windEl2)
+        humidEl2.innerHTML = "Humidity: " + response.list[1].main.humidity + "%";
+        forecast2.append(humidEl2)
+
+        //day 3 forecast  
+        var iconEl3 = document.createElement("img");
+        let icon3 = response.list[2].weather[0].icon;
+        iconEl3.setAttribute("src", "https://openweathermap.org/img/wn/" + icon3 + "@2x.png");
+        iconEl3.setAttribute("alt", response.list[2].weather[0].description);
+        forecast3.append(icon3);
+
+        var tempEl3 = document.createElement("p");
+        var windEl3 = document.createElement("p");
+        var humidEl3 = document.createElement("p");
+
+        tempEl3.innerHTML = "Temp: " + response.list[2].main.temp + "F";
+        forecast3.append(tempEl3)
+        windEl3.innerHTML = "Wind: " + response.list[2].wind.speed + " MPH";
+        forecast3.append(windEl3)
+        humidEl3.innerHTML = "Humidity: " + response.list[2].main.humidity + "%";
+        forecast3.append(humidEl3)
+
+        //day 4
+        var iconEl4 = document.createElement("img");
+        let icon4 = response.list[3].weather[0].icon;
+        iconEl4.setAttribute("src", "https://openweathermap.org/img/wn/" + icon4 + "@2x.png");
+        iconEl4.setAttribute("alt", response.list[3].weather[0].description);
+        forecast4.append(icon4);
+
+        var tempEl4 = document.createElement("p");
+        var windEl4 = document.createElement("p");
+        var humidEl4 = document.createElement("p");
+
+        tempEl4.innerHTML = "Temp: " + response.list[3].main.temp + "F";
+        forecast4.append(tempEl4)
+        windEl4.innerHTML = "Wind: " + response.list[3].wind.speed + " MPH";
+        forecast4.append(windEl4)
+        humidEl4.innerHTML = "Humidity: " + response.list[3].main.humidity + "%";
+        forecast4.append(humidEl4)
+
+        //day5
+                
+        var iconEl5 = document.createElement("img");
+        let icon5 = response.list[4].weather[0].icon;
+        iconEl5.setAttribute("src", "https://openweathermap.org/img/wn/" + icon5 + "@2x.png");
+        iconEl5.setAttribute("alt", response.list[4].weather[0].description);
+        forecast5.append(icon5);
+
+        var tempEl5 = document.createElement("p");
+        var windEl5 = document.createElement("p");
+        var humidEl5 = document.createElement("p");
+
+        tempEl5.innerHTML = "Temp: " + response.list[4].main.temp + "F";
+        forecast5.append(tempEl5)
+        windEl5.innerHTML = "Wind: " + response.list[4].wind.speed + " MPH";
+        forecast5.append(windEl5)
+        humidEl5.innerHTML = "Humidity: " + response.list[4].main.humidity + "%";
+        forecast5.append(humidEl5)
+     })
 }
-console.log(localStorage)
-
-//creating search history
+// //creating search history
 function renderSearchHistory () {
     list.innerHTML = "";
     for(let i = 0; i < searchHistory.length; i++) {
